916ee9e550b47d5385c916973145db8802f0c524
gothiquess <goldengoth.mail@gmail.com>
2025-02-09 18:29:28 -0500

    plover: add plugin

--- home/users/ess/apps/plover/default.nix
+++ home/users/ess/apps/plover/default.nix
@@ -5,12 +5,15 @@
 }: let
   xkbcommon = inputs.nixpkgs-xkbcommon.legacyPackages.${pkgs.system}.python311Packages.xkbcommon;
 in {
-  home.packages = with pkgs; [
+  home.packages = with pkgs;
+  with libsForQt5;
+  with qt5; [
     # Plugins.
     (inputs.plover-flake.packages.${pkgs.system}.plover.with-plugins
       (ps:
         with ps; [
           plover-dict-commands
+          plover-stitching
           plover-svg-layout-display
           plover-word-tray
           (plover-uinput.overrideAttrs (old: {
@@ -21,6 +24,6 @@ in {
         ]))
     # Required to work in Wayland until PR is done on master...
     # CC: https://github.com/openstenoproject/plover/releases/tag/continuous
-    libsForQt5.qt5.qtwayland
+    qtwayland
   ];
 }
