63d3b6d40ff02616420fb52e8ea0930b47c14dc2
gothiquess <goldengoth.mail@gmail.com>
2025-02-10 19:18:41 -0500

    theme: update

--- theme/harmony.nix
+++ theme/harmony.nix
@@ -1,40 +1,23 @@
-{
-  lib,
-  palette,
-}: let
+{palette}: let
   pick-color = colorlist: builtins.elemAt colorlist;
-  red = position: pick-color (palette.red) position;
   orange = position: pick-color (palette.orange) position;
-  amber = position: pick-color (palette.amber) position;
-  yellow = position: pick-color (palette.yellow) position;
-  lime = position: pick-color (palette.lime) position;
-  green = position: pick-color (palette.green) position;
   emerald = position: pick-color (palette.emerald) position;
-  teal = position: pick-color (palette.teal) position;
-  cyan = position: pick-color (palette.cyan) position;
-  sky = position: pick-color (palette.sky) position;
-  blue = position: pick-color (palette.blue) position;
   indigo = position: pick-color (palette.indigo) position;
-  violet = position: pick-color (palette.violet) position;
-  purple = position: pick-color (palette.purple) position;
-  fuchsia = position: pick-color (palette.fuhcsia) position;
-  pink = position: pick-color (palette.pink) position;
   rose = position: pick-color (palette.rose) position;
-  slate = position: pick-color (palette.slate) position;
-  gray = position: pick-color (palette.gray) position;
-  zinc = position: pick-color (palette.zinc) position;
-  neutral = position: pick-color (palette.neutral) position;
   stone = position: pick-color (palette.stone) position;
-  sand = position: pick-color (palette.sand) position;
-  olive = position: pick-color (palette.olive) position;
-  mauve = position: pick-color (palette.mauve) position;
 in {
   colors = {
     primary = rec {
-      bg = emerald 0;
-      fg = emerald 10;
-      focused = emerald 5;
+      bg = orange 0;
+      fg = orange 10;
+      focused = orange 5;
       inactive = bg;
+      highlight = orange 1;
+      match = orange 2;
     };
+  };
+  fonts = {
+    sans = "Manrope Medium";
+    serif = "";
   };
 }
