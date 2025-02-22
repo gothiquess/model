157c818b6760de4e65cc6c8dfa465396eec55965
gothiquess <goldengoth.mail@gmail.com>
2025-02-21 20:15:00 -0500

    theme: update

--- theme/harmony.nix
+++ theme/harmony.nix
@@ -1,20 +1,20 @@
 {palette}: let
   pick-color = colorlist: builtins.elemAt colorlist;
   zinc = position: pick-color (palette.zinc) position;
-  emerald = position: pick-color (palette.emerald) position;
+  rose = position: pick-color (palette.rose) position;
 in {
   colors = {
     primary = rec {
       bg = zinc 1;
       fg = zinc 8;
-      focused = zinc 9;
+      focused = zinc 5;
       inactive = bg;
       highlight = zinc 1;
       match = zinc 2;
     };
     secondary = {
-      focused = emerald 4;
-      inactive = emerald 7;
+      focused = rose 4;
+      inactive = rose 7;
     };
   };
   fonts = {
